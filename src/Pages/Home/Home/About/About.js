af3858db8825b857f69d38eed3ba5d2d6544255d
ashimmollick <ashimmollick46@gmail.com>
2023-10-04 12:05:00 +0600

    some resume uploading

--- src/Pages/Home/Home/About/About.js
+++ src/Pages/Home/Home/About/About.js
@@ -13,10 +13,10 @@ const About = () => {
 
 
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://placeimg.com/260/400/arch " className="max-w-sm rounded-lg shadow-2xl" />
+                <img src="https://placeimg.com/260/400/arch " className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Full Stack Web Developer</h1>
-                        <p className="py-6">I am a self-motivated Full Stack Developer with knowledge in React.js, Node.js, Express.js, Google Authentication, MongoDB, JavaScript, HTML, CSS, Bootstrap, Tailwind and mobile responsive web development.</p>
+                    <h1 className="text-5xl font-bold">Full Stack Web Developer</h1>
+                    <p className="py-6">I am a self-motivated Full Stack Developer with knowledge in React.js, Node.js, Express.js, Google Authentication, MongoDB, JavaScript, HTML, CSS, Bootstrap, Tailwind and mobile responsive web development.</p>
                         <button className="btn btn-outline btn-secondary"><a href={resume} download="resume">Resume</a></button>
                     </div>
 
